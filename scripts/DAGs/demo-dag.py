b6b3b31a1747cc91e3f5ac841783d05d0172bcf8
Iskandar Khudzhamkulov <ikhudzhamkul_c@ngs.org>
2023-06-18 14:30:58 +0600

    Deleted service account from trigger

--- scripts/DAGs/demo-dag.py
+++ scripts/DAGs/demo-dag.py
@@ -3,9 +3,11 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
+
 # Define your custom Python function
 def print_hello():
-    print("Hello, Airflow!")
+    print("Hello, Airflow!!")
+
 
 # Define the DAG
 default_args = {
@@ -15,16 +17,16 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('sample_dag', default_args=default_args, schedule_interval=timedelta(days=1))
+dag = DAG('sample_dag',
+          default_args=default_args,
+          schedule_interval=timedelta(days=1))
 
 # Define the tasks
 start_task = DummyOperator(task_id='start_task', dag=dag)
 
-hello_task = PythonOperator(
-    task_id='hello_task',
-    python_callable=print_hello,
-    dag=dag
-)
+hello_task = PythonOperator(task_id='hello_task',
+                            python_callable=print_hello,
+                            dag=dag)
 
 end_task = DummyOperator(task_id='end_task', dag=dag)
 
